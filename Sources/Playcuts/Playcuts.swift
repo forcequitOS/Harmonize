e0ce7201be38955d0bdaa6935c73761c7f3fc7f9
forcequitOS <62081155+forcequitOS@users.noreply.github.com>
2024-08-10 15:32:55 -0400

    Getting somewhere.

--- Sources/Playcuts/Playcuts.swift
+++ Sources/Playcuts/Playcuts.swift
@@ -1,101 +1,100 @@
 import SwiftUI
 import Swifter
 
-public struct PlaycutsConfiguration {
-    public var port: UInt16
-
-    public init(port: UInt16 = 6996) {
-        self.port = port
-    }
-}
-
 public class Playcuts: ObservableObject {
-    public static let shared = Playcuts()
-
-    @AppStorage("pairingCode") public var pairingCode: Int = 0
-    public var server: HttpServer?
-    public var isRunning = false
+    @AppStorage("pairingCode") private var pairingCode: Int = 0
+    @Published public var isRunning = false
     @Published public var receivedDisplayData: String = ""
     @Published public var isPairingCodeVisible = false
-    @Published public var showingResetAlert = false
-
-    // Change initializer to public
-    public init() {}
-
-    public func start(configuration: PlaycutsConfiguration = PlaycutsConfiguration()) {
-        guard !isRunning else { return }
+    
+    private var server: HttpServer?
+    
+    public init() {
+        // Generate a new pairing code if it hasn't been generated yet
+        if pairingCode == 0 {
+            resetPairingCode()
+        }
+    }
+    
+    public func start() {
+        guard server == nil else { return } // Server already running
+        
         let server = HttpServer()
         server.listenAddressIPv4 = "127.0.0.1"
-
-        server["/"] = { [weak self] request in
-            guard let self = self else { return HttpResponse.internalServerError }
-
+        let port: UInt16 = 6996
+        
+        server["/"] = { request in
             guard let jsonData = Data(request.body) as? Data,
                   let json = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any] else {
                 return HttpResponse.badRequest(.text("Invalid JSON"))
             }
-
+            
             guard let authCode = json["auth"] as? Int else {
                 print("No pairing code detected.")
-                return HttpResponse.badRequest(.text("Your request is missing a pairing code. Assuming you're a developer, you can correct this by providing an auth key with a simple integer value matching the current pairing code."))
+                return HttpResponse.badRequest(.text("Your request is missing a pairing code."))
             }
-
+            
             let verification = json["verification"] as? Bool
             let display = json["display"] as? String
-
-            var consoleLog = json
-            consoleLog.removeValue(forKey: "auth")
-
+            
             if authCode == self.pairingCode {
                 if verification == true {
                     print("Successfully authenticated!")
                     return HttpResponse.ok(.text("Success"))
                 } else if verification == nil {
                     if let displayData = display {
-                        print("Received: \(consoleLog)")
-                        self.receivedDisplayData = displayData
+                        print("Received: \(json)")
+                        DispatchQueue.main.async {
+                            self.receivedDisplayData = displayData
+                        }
                         return HttpResponse.ok(.text("Request received successfully!"))
                     } else {
-                        print("Received: \(consoleLog)")
+                        print("Received: \(json)")
                         return HttpResponse.badRequest(.text("Request received but no 'display' key found."))
                     }
                 }
             } else {
-                print("Uh-oh! Invalid pairing code.")
-                return HttpResponse.badRequest(.text("Your pairing code is incorrect. Please verify that your pairing code is the same across this app and Shortcuts."))
+                print("Invalid pairing code.")
+                return HttpResponse.badRequest(.text("Your pairing code is incorrect."))
             }
-
-            print("Received: \(consoleLog)")
-            print("This request cannot be used by the app.")
+            
             return HttpResponse.badRequest(.text("Invalid request!"))
         }
-
+        
         do {
-            try server.start(configuration.port, forceIPv4: true)
-            print("Server initialized successfully on port \(configuration.port)!")
+            try server.start(port, forceIPv4: true)
+            print("Server initialized successfully on port \(port)!")
             self.server = server
-            self.isRunning = true
+            DispatchQueue.main.async {
+                self.isRunning = true
+                self.displayPairingCode()
+            }
         } catch {
             print("Server initialization error: \(error)")
         }
     }
-
+    
     public func stop() {
-        guard isRunning else { return }
         server?.stop()
         server = nil
-        isRunning = false
+        DispatchQueue.main.async {
+            self.isRunning = false
+        }
         print("Server has stopped")
     }
-
+    
     public func displayPairingCode() {
-        self.isPairingCodeVisible = true
+        withAnimation {
+            isPairingCodeVisible = true
+        }
     }
-
+    
     public func dismissPairingCode() {
-        self.isPairingCodeVisible = false
+        withAnimation {
+            isPairingCodeVisible = false
+        }
     }
-
+    
     public func resetPairingCode() {
         pairingCode = Int.random(in: 1...9999)
         displayPairingCode()
@@ -103,28 +102,28 @@ public class Playcuts: ObservableObject {
 }
 
 public struct PairingCodeOverlay: View {
-    public let pairingCode: String
-    public let onDismiss: () -> Void
-
+    let pairingCode: String
+    let onDismiss: () -> Void
+    
     public init(pairingCode: String, onDismiss: @escaping () -> Void) {
         self.pairingCode = pairingCode
         self.onDismiss = onDismiss
     }
-
+    
     public var body: some View {
         ZStack {
             VisualEffectBlur(style: .systemUltraThinMaterial)
-
+            
             VStack(spacing: 20) {
                 Text("Pairing Code")
                     .font(.system(.largeTitle))
                     .fontWeight(.semibold)
                     .frame(maxWidth: .infinity)
-
+                
                 Text("Shortcuts Setup")
                     .font(.system(.title2))
                     .frame(maxWidth: .infinity)
-
+                
                 HStack(spacing: 15) {
                     ForEach(pairingCode.map { String($0) }, id: \.self) { digit in
                         Text(digit)
@@ -142,19 +141,16 @@ public struct PairingCodeOverlay: View {
         .onTapGesture {
             onDismiss()
         }
+        .transition(.opacity)
     }
 }
 
 public struct VisualEffectBlur: UIViewRepresentable {
     public var style: UIBlurEffect.Style = .systemMaterial
-
-    public init(style: UIBlurEffect.Style = .systemMaterial) {
-        self.style = style
-    }
-
+    
     public func makeUIView(context: Context) -> UIVisualEffectView {
         return UIVisualEffectView(effect: UIBlurEffect(style: style))
     }
-
+    
     public func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
 }
