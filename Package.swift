60738e9f43e82ce2f5ec320b8a37b4425ad950a7
forcequitOS <62081155+forcequitOS@users.noreply.github.com>
2024-10-12 23:51:42 -0400

    Update Package.swift to new name

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Playcuts",
+    name: "Harmonize",
     platforms: [
         .iOS(.v17)
     ],
     products: [
         .library(
-            name: "Playcuts",
-            targets: ["Playcuts"]),
+            name: "Harmonize",
+            targets: ["Harmonize"]),
     ],
     dependencies: [
         .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0"),
     ],
     targets: [
         .target(
-            name: "Playcuts",
+            name: "Harmonize",
             dependencies: [
                 .product(name: "Swifter", package: "swifter")
             ],
